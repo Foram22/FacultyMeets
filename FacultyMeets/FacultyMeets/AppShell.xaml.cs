6a0d42676addbd33a94a3b7368ffa0d770e383fa
Foram22 <foramjpatel5778@gmail.com>
2023-06-13 17:35:15 -0400

    Initial Commit

---
+++ FacultyMeets/FacultyMeets/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FacultyMeets;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ FacultyMeets/FacultyMeets/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FacultyMeets;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
