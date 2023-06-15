f5ad98a6dafc5425811d902a68a63c049850e9ba
Foram22 <foramjpatel5778@gmail.com>
2023-06-15 15:08:57 -0400

    Two new pages added LoginPage and RefgisterPage

---
+++ FacultyMeets/FacultyMeets/Platforms/iOS/ToastService.cs
@@ -0,0 +1,15 @@
+﻿namespace FacultyMeets.Platforms.Android;
+
+public class ToastService : ContentPage
+{
+	public ToastService()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
