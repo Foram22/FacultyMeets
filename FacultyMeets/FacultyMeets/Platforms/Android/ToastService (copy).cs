ff263606ed9bf84cc9d2b4c2e1c1294d064e81f7
Foram22 <foramjpatel5778@gmail.com>
2023-06-15 17:03:50 -0400

    Firebase packages added into the project

---
+++ FacultyMeets/FacultyMeets/Platforms/Android/ToastService (copy).cs
@@ -0,0 +1,15 @@
+﻿using Android.Widget;
+using FacultyMeets;
+using Xamarin;
+
+[assembly: Dependency(typeof(IToastService))]
+namespace YourNamespace.Droid
+{
+    public class ToastService : IToastService
+    {
+        public void ShowToast(string message)
+        {
+            Toast.MakeText(Android.App.Application.Context, message, ToastLength.Short).Show();
+        }
+    }
+}
