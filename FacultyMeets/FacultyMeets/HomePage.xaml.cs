847e030a940d0455bb75312ef5ee0cdfe7cf2328
Foram22 <foramjpatel5778@gmail.com>
2023-07-14 17:51:36 -0400

    home page added for the selection of role of user between faculty and student.

---
+++ FacultyMeets/FacultyMeets/HomePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace FacultyMeets;
+
+public partial class HomePage : ContentPage
+{
+    string rb_text = "";
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCheckedChange(object sender, CheckedChangedEventArgs e)
+    {
+        // Handle radio button checked event
+        var radioButton = (RadioButton)sender;
+        rb_text = radioButton.Content.ToString();
+        Console.WriteLine($"Selected option: {rb_text}");
+    }
+
+    private void OnContinueClicked(object sender, EventArgs e)
+    {
+        // Perform appropriate navigation or logic based on the selected user (faculty or student)
+        if (rb_text == "Faculty")
+        {
+            //DependencyService.Get<IToastService>().ShowToast(selectedUser);
+            Console.WriteLine($"Selected option: {rb_text}");
+        }
+        else if (rb_text == "Student")
+        {
+            //DependencyService.Get<IToastService>().ShowToast(selectedUser);
+            Console.WriteLine($"Selected option: {rb_text}");
+        }
+    }
+}
--- FacultyMeets/FacultyMeets/RegisterPage.xaml.cs
+++ FacultyMeets/FacultyMeets/RegisterPage.xaml.cs
@@ -18,7 +18,7 @@ public partial class RegisterPage : ContentPage
         // You can use authentication services like Firebase Authentication or ASP.NET Identity
 
         // Navigate to the home page after successful registration
-        // Navigation.PushAsync(new HomePage());
+        Navigation.PushAsync(new HomePage());
     }
 
     private async void OnLoginClicked(object sender, EventArgs e)
